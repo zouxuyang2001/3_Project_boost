2bb0c24f1b4f9c196043f950ad8f2c8696530368
zouxuyang2001 <72120922+zouxuyang2001@users.noreply.github.com>
2020-10-01 00:23:55 -0400

    fixed the size of rocket,implement c# script for rocket,which will make it thrusting

---
+++ Assets/rocket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()    {
+        ProcessInput();
+    }
+
+   void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)){ //can thrust while rotating
+           rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A)) { 
+            print("rotating left");
+        }
+        
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotating right");
+        }
+
+
+    }
+}
