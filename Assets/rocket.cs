ebe6007c3761dca8e25ba52ec63806bdce59caf1
zouxuyang2001 <72120922+zouxuyang2001@users.noreply.github.com>
2020-10-01 18:27:27 -0400

    changed the color of rocket and ground,fixed the speed .

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -6,6 +6,8 @@ public class rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
     AudioSource thrusting;
+    [SerializeField] float rcsRotation = 200f;
+    [SerializeField] float mainThrust = 1500f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +24,33 @@ public class rocket : MonoBehaviour
 
     void ProcessInput()
     {
+        Thrusting();
+        Rotate();
+
+    }
+
+    void Rotate()
+    {
+        float rotateThisFrame = rcsRotation * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+            transform.Rotate(Vector3.forward*rotateThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotateThisFrame);
+        }
+    }
+
+    void Thrusting()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         { //can thrust while rotating
-            rigidbody.AddRelativeForce(Vector3.up);
+            
+            rigidbody.AddRelativeForce(Vector3.up*thrustThisFrame);
 
             if (!thrusting.isPlaying)
             {
@@ -35,19 +61,5 @@ public class rocket : MonoBehaviour
         {
             thrusting.Stop();
         }
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(Vector3.forward);
-            }
-
-            else if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(-Vector3.forward);
-            }
-
-
-        
     }
-
 }
