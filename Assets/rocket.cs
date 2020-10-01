2629cab01153185c426f1ecf2bda199ebaac7080
zouxuyang2001 <72120922+zouxuyang2001@users.noreply.github.com>
2020-10-01 18:10:19 -0400

    fixed the rocket speed,make sure the speed wont change in diferent pc.

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -6,7 +6,8 @@ public class rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
     AudioSource thrusting;
-
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 1500f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +23,33 @@ public class rocket : MonoBehaviour
 
     void ProcessInput()
     {
+        Thrust();
+        Rotate();
+
+    }
+
+    void Rotate() //player can rotate rocket
+    {
+        rigidbody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidbody.freezeRotation = false; // resume physics control of rotation 
+    }
+
+    void Thrust() //player can make rocket move
+    {
         if (Input.GetKey(KeyCode.Space))
         { //can thrust while rotating
-            rigidbody.AddRelativeForce(Vector3.up);
+            float thrustThisFrame = mainThrust * Time.deltaTime;
+            rigidbody.AddRelativeForce(Vector3.up*thrustThisFrame);
 
             if (!thrusting.isPlaying)
             {
@@ -35,19 +60,5 @@ public class rocket : MonoBehaviour
         {
             thrusting.Stop();
         }
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(Vector3.forward);
-            }
-
-            else if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(-Vector3.forward);
-            }
-
-
-        
     }
-
 }
