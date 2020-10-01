8cf57905871e37db0399e21ff5966f448200da55
zouxuyang2001 <72120922+zouxuyang2001@users.noreply.github.com>
2020-10-01 16:24:05 -0400

    put the audio sound thrusting in rocket.cs

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -5,33 +5,49 @@ using UnityEngine;
 public class rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
+    AudioSource thrusting;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        thrusting = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()    {
+    void Update()
+    {
         ProcessInput();
     }
 
-   void ProcessInput()
+    void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)){ //can thrust while rotating
-           rigidbody.AddRelativeForce(Vector3.up);
-        }
+        if (Input.GetKey(KeyCode.Space))
+        { //can thrust while rotating
+            rigidbody.AddRelativeForce(Vector3.up);
 
-        if (Input.GetKey(KeyCode.A)) { 
-            print("rotating left");
+            if (!thrusting.isPlaying)
+            {
+                thrusting.Play();
+            }
         }
-        
-        else if (Input.GetKey(KeyCode.D))
+        else
         {
-            print("rotating right");
+            thrusting.Stop();
         }
 
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward);
+            }
 
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(-Vector3.forward);
+            }
+
+
+        
     }
+
 }
